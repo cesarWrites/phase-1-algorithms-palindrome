5c7c4f5ea9ea17028119da0dfa4537b197cb5ebc
Sarah Mukuti <cesarwrites24@gmail.com>
2022-06-17 22:03:44 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const len = word.length;
+
+
+  for (let i = 0; i < len / 2; i++) {
+
+      if (word[i] === word[len - 1 - i]) {
+          return true;
+      }else{
+          return false;
+      }
+  }
+  
 }
+console.log(isPalindrome('abba'))
 
 /* 
   Add your pseudocode here
+  input the string to test
+  get length of teh string
+  if the new string is read similar to the original
+  then plindrome is true
+  else
+  the word is not a palindrome
+
 */
 
 /*
   Add written explanation of your solution here
+  The code checks the length of the
+   string and compared it to the new string
+   if the last and first characters are teh same returns a plaindrome 
+   else returns not palindrome
+  I
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +45,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+
 }
 
 module.exports = isPalindrome;
